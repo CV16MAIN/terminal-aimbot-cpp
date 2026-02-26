0b0a44203535d7c77292cf8f74f4916bdbbeebdf
VOIDMAIN17 <cv16main@gmail.com>
2026-02-26 17:55:48 +0530

    Update log.h

--- log.h
+++ log.h
@@ -1,1 +1,4 @@
+#pragma once  // this we use so that the program only process .h file once only we can also use ifndef _Log_Main_ endif some thing like this to but no one use it now days 
 
+
+int userMainInput();

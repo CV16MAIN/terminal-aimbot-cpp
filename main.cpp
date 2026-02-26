4145789cc58373339755b5dda32385bea3120a94
VOIDMAIN17 <cv16main@gmail.com>
2026-02-26 17:56:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+//TERMINAL AIMBOT 
+#include <iostream> // why iostream cause we want to include the input and out of c++ standard lib and this iostream is also a header ... so why we don't write 
+// include <iostream.h> casue this is the part of c hence it don't take h where as which files that have 'h' are the part of new c++ 
+#include "log.h"
 
+int main() {
+	 userMainInput();
+	return 0;
+}
